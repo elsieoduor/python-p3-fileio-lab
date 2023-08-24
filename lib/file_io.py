ef4d75f5739d5d5079e5cda4ea5b2e7e2a088128
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-24 07:55:05 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as x:
+        x.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as x:
+        x.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as x:
+        return x.read()
